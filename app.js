92c0bdedd9d818c6d630afd059e3c28b486da520
minhthn <chemminh2k@gmail.com>
2023-09-09 22:08:05 +0700

    add error handler

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const express = require('express');
 const cors = require('cors');
 
 const contactsRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/api-error');
 
 const app = express();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
 
+// routes
 app.use('/api/contacts', contactsRouter);
+
+// error handler middlewares
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'));
+});
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal server error'
+    });
+});
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello, this is contact book application.' });
